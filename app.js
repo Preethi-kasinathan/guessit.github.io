a6f91e709e992e3930a245d8e707693c30c4b4d3
Preethi-kasinathan <61906137+Preethi-kasinathan@users.noreply.github.com>
2020-03-21 19:59:05 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+var i;
+var mysound;
+
+function check_1() {
+
+    alert('WoW               Correct Answer');
+
+    function preload() {
+
+        mySound = new sound("audio/Correct-answer.mp3");
+        mysound.play();
+
+    }
+}
+
+
+function wrong_1() {
+    alert('Ohhh no..    Its wrong!!!');
+}
